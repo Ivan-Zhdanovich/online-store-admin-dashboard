0ad9936e090f3464e9f37f9dd0ad263f19a358ee
Zhdanovich <Zhiv80@mail.ru>
2025-09-19 13:09:44 +0300

    feat: add main header with styles, pages routes, add libs

---
+++ src/app/(home)/components/mainHeader/page.tsx
@@ -0,0 +1,34 @@
+import { MainPath } from "@/services/router/rotes";
+import Link from "next/link";
+import styles from "./mainHeader.module.scss";
+
+export default function MainHeader() {
+	const renderNavigationElements = () => {
+		return (
+			<>
+				<Link className={styles.navigationLink} href={MainPath.Users}>
+					User management
+				</Link>
+				<Link className={styles.navigationLink} href={MainPath.Products}>
+					<p className={styles.navigationLink__text}>Product management</p>
+				</Link>
+				<Link className={styles.navigationLink} href={MainPath.Orders}>
+					<p className={styles.navigationLink__text}>Order Management</p>
+				</Link>
+				<Link className={styles.navigationLink} href={MainPath.Reports}>
+					<p className={styles.navigationLink__text}>Sales Management</p>
+				</Link>
+				<Link className={styles.navigationLink} href={MainPath.Analytics}>
+					<p className={styles.navigationLink__text}>Analytics</p>
+				</Link>
+			</>
+		);
+	};
+	return (
+		<header className={styles.headerWrap}>
+			<div className={styles.headerContainer}>
+				<nav className={styles.navigationWrap}>{renderNavigationElements()}</nav>
+			</div>
+		</header>
+	);
+}
---
+++ src/services/router/rotes.ts
@@ -0,0 +1,8 @@
+export enum MainPath {
+	Main = "/",
+	Users = "/admin/users",
+	Products = "/admin/products",
+	Orders = "/admin/orders",
+	Reports = "/admin/reports",
+	Analytics = "/admin/analytics",
+}
